b5b0399b5c4f7df071598a5d94c3c97c3a51f795
vladimir <vladimir.vlkgn@mail.ru>
2019-01-12 21:04:28 +0300

    In 21-05

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
+#include <iostream>
+
 #include "lib.h"
 
-#include <iostream>
-#include <spdlog/spdlog.h>
+
+//#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[])
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started",version());
+   // auto logger = spdlog::stdout_logger_mt("console");
+    //logger->info("version {} was started",version());
     std::cout<<"Hello, world!"<<std::endl;
+    std::cout<<"version "<<version()<<std::endl;
 
     return 0;
 }
