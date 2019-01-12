3d41576e00d95061aaf8e4ed417141fdd1a6ed1b
vladimir <vladimir.vlkgn@mail.ru>
2019-01-12 15:00:22 +0300

    First commit helloworld

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "lib.h"
+
+#include <spdlog/spdlog.h>
+
+int main(int argc, char *argv[])
+{
+    auto logger = spdlog::stdout_logger_mt("console");
+    logger->info("version {} was started",version());
+
+
+    return 0;
+}
