07ed45c7a519f35d86eea87a78002d37dbd82951
vladimir <vladimir.vlkgn@mail.ru>
2019-01-12 20:53:23 +0300

    In 20-50

--- test.cpp
+++ test.cpp
@@ -1,12 +1,14 @@
-#define BOOST_TEST_MODULE test_version
+#define BOOST_TEST_MODULE test
 
 #include "lib.h"
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(test_suite)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
     BOOST_CHECK(version() > 0);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
