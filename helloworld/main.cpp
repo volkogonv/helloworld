8c5a43ecbbde69894c6110bf48abea8fcfbe01ff
vladimir <vladimir.vlkgn@mail.ru>
2019-01-12 15:48:55 +0300

    №3

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,12 +1,12 @@
 #include "lib.h"
 
 #include <iostream>
-//#include <spdlog/spdlog.h>
+#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[])
 {
-   // auto logger = spdlog::stdout_logger_mt("console");
-   // logger->info("version {} was started",version());
+    auto logger = spdlog::stdout_logger_mt("console");
+    logger->info("version {} was started",version());
     std::cout<<"Hello, world!"<<std::endl;
 
     return 0;
